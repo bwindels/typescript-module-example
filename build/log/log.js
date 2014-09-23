2959d9c134faa8501efee5b761f4db4856779b32
Bruno Windels <bruno.windels@gmail.com>
2014-09-23 17:28:17 +0200

    dont commit build folder

--- build/log/log.js
+++
@@ -1,4 +0,0 @@
-function logSomething(foo) {
-    console.log('log', foo.a, foo.b);
-}
-exports.logSomething = logSomething;
--- build/main/main.js
+++
@@ -1,5 +0,0 @@
-var log = require('log/log');
-var math = require('./math');
-
-log.logSomething({ a: "yo", b: 3.141592 });
-console.log(math.sum(5, 2));
